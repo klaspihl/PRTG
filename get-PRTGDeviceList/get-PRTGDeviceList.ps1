63b86efbb4eeb647d2e5d7043338da85563774fe
Klas Pihl <klas.pihl@atea.se>
2023-02-15 14:38:31 +0100

    Add support for API token key and specific device

--- get-PRTGDeviceList/get-PRTGDeviceList.ps1
+++ get-PRTGDeviceList/get-PRTGDeviceList.ps1
@@ -10,7 +10,7 @@
         PowerShell credential request
         User and password to PRTG web GUI
         Password for user PersonalUser: **********
-
+.EXAMPLE 
     PS C:\> get-PRTGDeviceList -PRTGCore https://monitor.pihl.local
         cmdlet get-PRTGDeviceList.ps1 at command pipeline position 1
         Supply values for the following parameters:
@@ -18,6 +18,10 @@
         PowerShell credential request
         User and password to PRTG web GUI
         Password for user prtgadmin: **********
+
+.EXAMPLE
+   PS C:\>  .\get-PRTGDeviceList.ps1 -PRTGServer "https://prtg.pihl.local/"  -DeviceFQDN pi3w.pihl.local -APIkey 4TZNQQ...DNSA======
+    Get object data like ID from device with hostname pi3w.pihl.local
 
 .PARAMETER PRTGCore
     PRTG core server URL
@@ -29,6 +33,11 @@
 .PARAMETER UserHash
     Passhash of user, if omitted user can enter credentials
     User can get passhash from "https://$PRTGHost/api/getpasshash.htm?username=myuser&password=mypassword"
+
+.PARAMETER APIkey
+    PRTG User API key
+    If a valid path is entered file content is loaded as API Key
+
 .OUTPUTS
     Object of devices grouped on PRTG goups
     ID - ID of device
@@ -46,39 +55,84 @@
     9558 Lab                    pi3.pihl.local (pi3) [Linux/Unix]      pi3.pihl.local
 .NOTES
     Version 1 2021-06-04 Klas.Pihl@Atea.se
+    Version 2.0 2023-02-15 Add support for API token key and more general usage. /Klas.Pihl@Atea.se
 #>
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory=$true,HelpMessage="PRTG server URL")]
+    [Parameter(
+        Mandatory=$true,
+        HelpMessage="PRTG server URL"
+        )]
+    [Alias("PRTGServer")]
     [uri]$PRTGCore,
-    [Parameter(Mandatory=$true,HelpMessage="User account name")]
+
+    [Parameter(
+        Mandatory=$false,
+        HelpMessage="User account name",
+        ParameterSetName = 'User'
+        )]
     [string]$UserName,
-    [Parameter(Mandatory=$false,HelpMessage="User account PRTG hash")]
-    [string]$UserHash
+
+    [Parameter(
+        Mandatory=$false,
+        HelpMessage="User account PRTG hash",
+        ParameterSetName = 'User'
+        )]
+    [string]$UserHash,
+
+    [Parameter(
+        Mandatory=$false,
+        HelpMessage="API Key path or key",
+        ParameterSetName = 'APIKey'
+        )]
+    [string]$APIkey='.\apikey.sec',
+
+    [Parameter(
+        Mandatory=$false,
+        HelpMessage="Device name FQDN"
+        )]
+    [string]$DeviceFQDN
 )
 $ErrorActionPreference = "Stop"
 try {
+    if(Test-Path $APIkey) {
+        Write-Verbose "Parameter APIKey is a valid file path, try to load API key from $APIKey"
+        $APIkey = Get-Content $APIkey -ErrorAction Stop
+    }
     if(-not $PRTGCore.IsAbsoluteUri -or
     -not (Test-NetConnection -ComputerName $PRTGCore.Host -Port $PRTGCore.port).TcpTestSucceeded
     ) {
         throw "Can not connect to PRTG Core server $($PRTGCore.AbsoluteUri)"
     }
     $PRTGCoreUri = $PRTGCore.AbsoluteUri.trimend('/')
-    if( -not ($PSBoundParameters.ContainsKey('UserHash'))) {
-        Write-Verbose "No passhash entered as argument, request user credentials and try to get hash from PRTG"
 
-        $Credentials = Get-Credential -Message "User and password to PRTG web GUI" -UserName $UserName
+    if($PSBoundParameters.ContainsKey('APIKey')) {
+        $urlAllDevices = "{0}/api/table.json?content=devices&output=json&columns=objid,group,device,host&id=0&count=2500&apitoken={1}" -f $PRTGCoreUri,$APIkey
 
-        $uriGetHash = "{0}/api/getpasshash.htm?username={1}&password={2}" -f $PRTGCoreUri,$Credentials.UserName,$Credentials.GetNetworkCredential().Password
-        $UserHash = Invoke-WebRequest $uriGetHash -verbose:$false | Select-Object -ExpandProperty Content
-        if([string]::IsNullOrEmpty($UserHash)) {
-            throw "Can not get user hash from PRTG server on user $($Credentials.UserName)"
+    } else {
+        if( -not ($PSBoundParameters.ContainsKey('UserHash'))) {
+            Write-Verbose "No passhash entered as argument, request user credentials and try to get hash from PRTG"
+    
+            $Credentials = Get-Credential -Message "User and password to PRTG web GUI" -UserName $UserName
+    
+            $uriGetHash = "{0}/api/getpasshash.htm?username={1}&password={2}" -f $PRTGCoreUri,$Credentials.UserName,$Credentials.GetNetworkCredential().Password
+            $UserHash = Invoke-WebRequest $uriGetHash -verbose:$false | Select-Object -ExpandProperty Content
+            if([string]::IsNullOrEmpty($UserHash)) {
+                throw "Can not get user hash from PRTG server on user $($Credentials.UserName)"
+            }
         }
-    }
+        $urlAllDevices = "{0}/api/table.json?content=devices&output=json&columns=objid,group,device,host&id=0&count=2500&username={1}&passhash={2}" -f $PRTGCoreUri,$UserName,$UserHash
 
-    $urlAllDevices = "{0}/api/table.xml?content=devices&output=csvtable&columns=objid,group,device,host&id=0&count=2500&username={1}&passhash={2}" -f $PRTGCoreUri,$Credentials.UserName,$UserHash
-    $alldevices = Invoke-WebRequest -Uri $urlAllDevices -verbose:$false | ConvertFrom-Csv -Delimiter ','
-    Write-Output $alldevices | Select-Object ID,Group,Device,Host | Sort-Object Group
+    }
+    $alldevices = Invoke-WebRequest -Uri $urlAllDevices -verbose:$false | ConvertFrom-Json | Select-Object -ExpandProperty devices
+    if($alldevices.count -ge 2499) {
+        Write-Warning "Number of devices exceed default limit of 2500 devices, might need special care"
+    }
+    if($PSBoundParameters.ContainsKey('DeviceFQDN')) {
+        Write-Output $alldevices | Select-Object objid,Group,Device,Host | Where-Object Host -eq $DeviceFQDN
+    } else {
+        Write-Output $alldevices | Select-Object objid,Group,Device,Host | Sort-Object Group
+    }
 } catch {
     write-error $psitem
 }
